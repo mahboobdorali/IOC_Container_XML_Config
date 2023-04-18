5f9c6bde188ea0b19c452b67c33798f421be7d6d
mahboobdorali <mahboobdorali@gmail.com>
2023-04-18 16:58:31 +0430

    add classes for practice

---
+++ src/main/java/org/example/HelloWorld.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class HelloWorld {
+    private String message;
+
+    public void getMessage() {
+        System.out.println("My Message : " + message);
+
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
